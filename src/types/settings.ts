526b48f85de1422a1e06b97489589534dff04bd6
Li Bowei <149855691+ActiveInsighter@users.noreply.github.com>
2026-06-26 22:31:17 +0800

    types: add extension settings

---
+++ src/types/settings.ts
@@ -0,0 +1,9 @@
+export interface ExtensionSettings {
+  autoScroll: boolean;
+  randomWait: boolean;
+  randomWaitMinMinutes: number;
+  randomWaitMaxMinutes: number;
+  supabaseEnabled: boolean;
+  supabaseUrl: string;
+  supabaseAnonKey: string;
+}
