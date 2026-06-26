21fa2ae8da38fdadc0ec7524929626b0c1d4e65a
Li Bowei <149855691+ActiveInsighter@users.noreply.github.com>
2026-06-26 22:45:56 +0800

    cleanup

--- extension/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('open-chatgpt')?.addEventListener('click', () => {
-  chrome.tabs.create({ url: 'https://chatgpt.com/' });
-});
-
-document.getElementById('open-options')?.addEventListener('click', () => {
-  chrome.runtime.openOptionsPage();
-});
